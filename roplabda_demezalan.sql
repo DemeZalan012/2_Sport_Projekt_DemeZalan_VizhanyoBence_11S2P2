8fee097f0bdfbb8b3913e00c433496383d30a2b8
Deme Zalán <deme.zalan.janos@diak.szbi-pg.hu>
2025-05-09 11:10:06 +0200

    Update and rename roplabda.sql to roplabda_demezalan.sql

--- roplabda_demezalan.sql
+++ roplabda_demezalan.sql
@@ -787,6 +787,8 @@ ALTER TABLE `vegeredmeny`
   ADD CONSTRAINT `vegeredmeny_ibfk_4` FOREIGN KEY (`gyoztes_csapat_id`) REFERENCES `csapatok` (`csapat_id`);
 COMMIT;
 
+//akinek el küldtem az használhatja- Deme Zalán
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
